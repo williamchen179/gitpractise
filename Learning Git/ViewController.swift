d4080ab30ab9e7579517af9befd1bd6927cd9af9
William Chen <williamchen1709@gmail.com>
2020-05-27 22:41:56 +1000

    Initial Commit

---
+++ Learning Git/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Learning Git
+//
+//  Created by William Chen on 27/5/20.
+//  Copyright © 2020 William Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
